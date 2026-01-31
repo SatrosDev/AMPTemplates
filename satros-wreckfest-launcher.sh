9602353a481249b3f08ef69045a8d9dbcd6b7b7a
SatrosDev <satroswar@gmail.com>
2026-01-31 16:57:56 -0500

    update messages and add mroe time to launcher

--- satros-wreckfest-launcher.sh
+++ satros-wreckfest-launcher.sh
@@ -7,11 +7,11 @@ set -m
 cleanup() {
     # echo "Cleanup initiated."
     if [ -n "$(jobs -r)" ]; then
-      echo "Background jobs are running..."
+      echo "[INFO] Background jobs are running."
       #close job if it failed
       if [ -n "$JOB_PID" ]; then
         #check for wf pid and kill?
-        echo "Stopping failed server launch attempt..."
+        echo "[INFO] Stopping failed server launch attempt..."
         #kill -9 -$JOB_PID #this works to close the jobs
         kill -TERM -$JOB_PID #this also works
         wait
@@ -23,23 +23,23 @@ trap cleanup INT TERM EXIT
 
 
 PORT="27016"
-INTERVAL=30 # Check port every 30 seconds
+INTERVAL=35 # Check port every 35 seconds
 
 while :
 do
-  echo "Checking if [Port $PORT] is in use..."
+  echo "[INFO] Checking if [Port $PORT] is being used."
   # Check if the port is open using ss
   if ! ss -tuln | grep -q ":$PORT\b"; then
 
     #If last job failed then close it
     if [ -n "$JOB_PID" ]; then
-      echo "Stopping failed server launch attempt..."
+      echo "[INFO] Stopping failed server launch attempt..."
       kill -TERM -$JOB_PID #this also works
       wait
-      sleep 1
-      echo "Relaunching Wreckfest Server..."
+      sleep 5
+      echo "[INFO] Relaunching Wreckfest Server..."
     else
-      echo "Starting Wreckfest Server..."
+      echo "[INFO] Starting Wreckfest Server..."
     fi
 
     # Server startup command
