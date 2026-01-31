3645377052443879cdc1c47524e88dd8af0a6424
SatrosDev <satroswar@gmail.com>
2026-01-31 12:13:41 -0500

    wreckfest launcher script

--- wreckfest-launcher.sh
+++ wreckfest-launcher.sh
@@ -1,35 +1,92 @@
 #!/bin/bash
 
+#set job control
+set -m
+
+
+cleanup() {
+    echo "Cleanup initiated."
+    if [ -n "$(jobs -r)" ]; then
+      echo "Background jobs are running."
+      echo "Killing background jobs..."
+      #close job if it failed
+      if [ -n "$JOB_PID" ]; then
+        #check for wf pid and kill?
+        echo "Killing process group -$JOB_PID with SIGTERM..."
+        #kill -9 -$JOB_PID #this works to close the jobs
+        kill -TERM -$JOB_PID #this also works
+        wait
+      else
+        echo "Variable 'JOB_PID' is unset"
+      fi
+    else
+      echo "No background jobs are running."
+    fi
+    exit
+}
+trap cleanup INT TERM EXIT
+
+#jobs -p | xargs -r kill
+
 # export WINEPREFIX=/home/wreckfest/wreckfest/
 # export WINEARCH=win64
 # export WINEDEBUG=-all
 # export XDG_RUNTIME_DIR=/tmp
 
 PORT="27016"
-INTERVAL=60 # Check every 30 seconds
+INTERVAL=30 # Check every 30 seconds
 
-wineserver -k
-screen -XS wreckfest quit
+# Clear any wreckfest stuff?
 
 while :
 do
-  # Check if the port is open using netstat or ss
-  # Use 'ss -tuln' or 'netstat -tuln' depending on your system
+  echo "Checking if [Port $PORT] is in use..."
+  # Check if the port is open using ss
   if ! ss -tuln | grep -q ":$PORT\b"; then
-    echo "Port $PORT is closed. Starting/Restarting program..."
-    wineserver -k
-    screen -XS wreckfest quit
 
-    screen -dmS wreckfest $@
-    #screen -dmS wreckfest xvfb-run -a wine Wreckfest.exe -s server_config=server_config.cfg log=/tmp/wreckfest.log
+    #close job if it failed
+    if [ -n "$JOB_PID" ]; then
+      #check for wf pid and kill?
+      echo "Killing process group -$JOB_PID with SIGTERM..."
+      #kill -9 -$JOB_PID #this works to close the jobs
+      kill -TERM -$JOB_PID #this also works
+      wait
+      sleep 10
+    else
+      echo "Variable 'JOB_PID' is unset"
+    fi
+
+    echo "Starting program... [Port $PORT is closed]"
+
+    #Run wreckfest startup command in the background
+    #echo $@
+    ($@) &
+    JOB_PID=$!
+    echo "Job (subshell) PID: $JOB_PID"
+    if [ -n "$JOB_PID" ]; then
+      echo "JOB_PIT was set: $JOB_PID"
+    else
+      echo "Variable 'JOB_PID' is unset"
+    fi
+
+    #xvfb-run -a wine Wreckfest.exe -s server_config=server_config.cfg &
+
+    echo "Program should be in the background... "
+    jobs
+    #screen -dmS wreckfest $@
+    #screen -dmS wreckfest xvfb-run -a wine Wreckfest.exe -s server_config=server_config.cfg
 	
   else
-    echo "Program is running and port $PORT is open... "
-    echo "Wreckfest sucessfully started... "
-    #screen -r wreckfest
+    echo "[SUCCESS] Program is running and port $PORT is open... "
+    echo "Wreckfest server started... "
     break
   fi
   sleep $INTERVAL
-  echo "checking port...."
 done
-screen -r wreckfest
+
+# Resume wreckfest
+jobs
+fg
+jobs
+#PID1=""
+#jobs
